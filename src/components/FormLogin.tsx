472ac34c5ca1bb0cd01dd986c448d0ed67fb50e3
Mishel Morales <mmejiam1825@gmail.com>
2026-02-07 22:23:59 -0600

    Se cambio el estilo de la page de login y descargo lo de sidebard

--- src/components/FormLogin.tsx
+++ src/components/FormLogin.tsx
@@ -36,27 +36,13 @@ const FormLogin = () => {
   })
 
   function onSubmit(data: z.infer<typeof validationLogin>) {
-    console.log(data, 'Que envia??')
-    // toast("You submitted the following values:", {
-    //   description: (
-    //     <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
-    //       <code>{JSON.stringify(data, null, 2)}</code>
-    //     </pre>
-    //   ),
-    //   position: "bottom-right",
-    //   classNames: {
-    //     content: "flex flex-col gap-2",
-    //   },
-    //   style: {
-    //     "--border-radius": "calc(var(--radius)  + 4px)",
-    //   } as React.CSSProperties,
-    // })
+    console.log(data, 'Que envia??');
   }
 
   return (
-    <Card className="w-full sm:max-w-md flex ">
+    <Card className="w-full sm:max-w-md flex border-0 shadow-none">
       <CardHeader>
-        <CardTitle className="text-center">Use your Account</CardTitle>
+        <CardTitle className="text-center text-cyan-500">Use your Account</CardTitle>
       </CardHeader>
       <CardContent>
         <form id="form-rhf-demo" onSubmit={form.handleSubmit(onSubmit)}>
@@ -68,14 +54,14 @@ const FormLogin = () => {
                 <Field data-invalid={fieldState.invalid}>
                   <Input
                     {...field}
-                    id="form-rhf-demo-title"
+                    id="form-rhf-demo-email"
                     aria-invalid={fieldState.invalid}
                     placeholder="Email"
                     autoComplete="off"
-                    className="placeholder:text-cyan-500 border-2 border-cyan-500 rounded-full text-sm"
+                    className="placeholder:text-cyan-500 border-2 border-cyan-500 rounded-full text-sm focus:border-cyan-600 focus-visible:ring-0"
                   />
                   {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
+                    <FieldError className="text-red-800" errors={[fieldState.error]} />
                   )}
                 </Field>
               )}
@@ -90,14 +76,15 @@ const FormLogin = () => {
                  
                   <Input
                     {...field}
-                    id="form-rhf-demo-title"
+                    id="form-rhf-demo-password"
                     aria-invalid={fieldState.invalid}
                     placeholder="Password"
+                    type="password"
                     autoComplete="off"
-                    className="placeholder:text-cyan-500 border-2 border-cyan-500 rounded-full text-sm"
+                    className="placeholder:text-cyan-500 border-2 border-cyan-500 rounded-full text-sm focus:border-cyan-600 focus-visible:ring-0"
                   />
                   {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
+                    <FieldError className="text-red-800" errors={[fieldState.error]} />
                   )}
                 </Field>
               )}
@@ -107,7 +94,7 @@ const FormLogin = () => {
       </CardContent>
       <CardFooter>
         <Field>
-          <Button type="submit" form="form-rhf-demo" className="bg-sky-400 rounded-full">
+          <Button type="submit" form="form-rhf-demo" className="bg-sky-400 rounded-full text-white">
             Submit
           </Button>
         </Field>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,14 +3,7 @@ import FormLogin from "@/components/FormLogin";
 const Login = () => {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="
-  h-64
-  w-full
-  bg-[url('/pez.jpg')]
-  bg-cover
-  bg-center
-" />
-
+      <div className="h-64 w-60 bg-[url('/pez.jpg')] bg-cover bg-center" />
       <FormLogin />
     </div>
   )
